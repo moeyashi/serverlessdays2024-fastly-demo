4d0e1160e53d5f6c1a19e1e379f39cd380807ac2
Ren Adachi <49895682+moeyashi@users.noreply.github.com>
2024-09-22 15:51:26 +0900

    refactor: モノレポに変更
    Signed-off-by: Ren Adachi <49895682+moeyashi@users.noreply.github.com>

--- kadai1/src/index.ts
+++ kadai1/src/index.ts
@@ -1,21 +1,38 @@
 /// <reference types="@fastly/js-compute" />
+import { Device } from "fastly:device";
 import { env } from "fastly:env";
 import { KVStore } from "fastly:kv-store";
 import { includeBytes } from "fastly:experimental";
 
 const editorPage = includeBytes("./src/editor.html");
+const listPage = includeBytes("./src/list.html");
 
 const CFG_SHORT_ID_LEN = 8;
 const CFG_PASSCODE = "BiRbc6Q4xkTHYGiM6ABhVon8mNmfiL";
 
+const KV_NAME = "url-shortner-js";
+const KV_KEY_LIST = "list";
+
 addEventListener("fetch", (event) => event.respondWith(handleRequest(event)));
 
-async function handleRequest(event: FetchEvent) {
+const handleRequest = (event: FetchEvent) => {
   console.log(
     "FASTLY_SERVICE_VERSION:",
     env("FASTLY_SERVICE_VERSION") || "local"
   );
+  const url = new URL(event.request.url);
+  switch (url.pathname) {
+    case "/":
+      return handleIndex(event);
+    case "/list":
+      return handleList(event);
+    default:
+      // 定義していないパスは短縮URLとして扱う
+      return handleIndex(event);
+  }
+};
 
+async function handleIndex(event: FetchEvent) {
   let req = event.request;
 
   if (
@@ -63,7 +80,11 @@ async function handleRequest(event: FetchEvent) {
         .replaceAll("-", "")
         .slice(-1 * CFG_SHORT_ID_LEN);
     }
-    const store = new KVStore("url-shortner-js");
+    const store = new KVStore(KV_NAME);
+    // const list = await store.get(KV_KEY_LIST);
+    // const oldList = list ? await list.json() : [];
+    // const newList = [...(Array.isArray(oldList) ? oldList : []), shortId];
+    // await store.put(KV_KEY_LIST, JSON.stringify(newList));
     await store.put(shortId, payload.url);
     return new Response(`{"short":"${shortId}"}`, {
       status: 201,
@@ -85,7 +106,7 @@ async function handleRequest(event: FetchEvent) {
           status: 500,
         });
       }
-      const store = new KVStore("url-shortner-js");
+      const store = new KVStore(KV_NAME);
       const location = (await (await store.get(path))?.text()) || "";
       if (location == "") {
         return new Response("redirect location not found", {
@@ -102,3 +123,25 @@ async function handleRequest(event: FetchEvent) {
     }
   }
 }
+
+const handleList = async (event: FetchEvent) => {
+  let req = event.request;
+  if (req.method != "GET") {
+    return new Response("This method is not allowed", {
+      status: 405,
+    });
+  }
+  const store = new KVStore(KV_NAME);
+  let keys = (await store.get(KV_KEY_LIST))?.json() || [];
+  let data = [];
+  for (let key of Array.isArray(keys) ? keys : []) {
+    data.push({
+      short: key,
+      url: (await (await store.get(key))?.text()) || "",
+    });
+  }
+  return new Response(JSON.stringify(data), {
+    status: 200,
+    headers: new Headers({ "Access-Control-Allow-Origin": "*" }),
+  });
+};
