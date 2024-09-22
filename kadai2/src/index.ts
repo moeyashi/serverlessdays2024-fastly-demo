fd709fa88e3747035978afacf43cdebb3da3ad95
Ren Adachi <49895682+moeyashi@users.noreply.github.com>
2024-09-22 16:19:28 +0900

    feat: markdownの変換をedgeで動かすように
    Signed-off-by: Ren Adachi <49895682+moeyashi@users.noreply.github.com>

--- kadai2/src/index.ts
+++ kadai2/src/index.ts
@@ -1,10 +1,21 @@
 /// <reference types="@fastly/js-compute" />
 
+import MarkdownIt from "markdown-it";
+
 addEventListener("fetch", (event) => event.respondWith(handleRequest(event)));
 
 async function handleRequest(event: FetchEvent) {
   let req = event.request;
-  return await fetch(req, {
-    backend: "backend_1",
-  });
+  let url = new URL(req.url);
+  if (req.method == "POST" && url.pathname == "/markdown-to-html") {
+    var md = new MarkdownIt();
+    return new Response(md.render(await req.text()), {
+      status: 200,
+      headers: new Headers({ "Content-Type": "text/html; charset=utf-8" }),
+    });
+  } else {
+    return await fetch(req, {
+      backend: "backend_1",
+    });
+  }
 }
